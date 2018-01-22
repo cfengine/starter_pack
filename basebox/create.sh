770b46e559017924546f10396424142062e1da83
Ole Herman Schumacher Elgesem <oleherman93@gmail.com>
2018-01-22 12:20:40 +0100

    Allow some errors in create scripts
    Signed-off-by: Ole Herman Schumacher Elgesem <oleherman93@gmail.com>

--- basebox/create.sh
+++ basebox/create.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
+vagrant destroy -f basebox
+rm -f base.box
+
 set -e
 set -x
 
-vagrant destroy -f basebox
 vagrant box update basebox
 vagrant up basebox
-rm -f base.box
 vagrant package basebox --output base.box
 vagrant destroy -f basebox
 vagrant box remove -f basebox
--- buildslave/create.sh
+++ buildslave/create.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
+vagrant destroy -f buildslavebox
+rm -f buildslave.box
+
 set -e
 set -x
 
-vagrant destroy -f buildslavebox
 vagrant box update buildslavebox
 vagrant up buildslavebox
-rm -f buildslave.box
 vagrant package buildslavebox --output buildslave.box
 vagrant destroy -f buildslavebox
 vagrant box remove -f buildslavebox
