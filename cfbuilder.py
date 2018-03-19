711ca551befcaabed1f280fd7c5fe65da3dab846
Ole Herman Schumacher Elgesem <ole.elgesem@northern.tech>
2018-03-19 21:56:28 +0100

    Started using yapf for python formatting
    Signed-off-by: Ole Herman Schumacher Elgesem <ole.elgesem@northern.tech>

--- cfbuilder.py
+++ cfbuilder.py
@@ -10,9 +10,11 @@ log = logging.getLogger(__name__)
 
 dry_run = False
 
+
 def user_error(msg):
     log.error(msg)
     sys.exit(1)
+
 
 def run_command(command, on_failure=None):
     if dry_run:
@@ -28,9 +30,13 @@ def run_command(command, on_failure=None):
             os.system(on_failure)
         sys.exit(r)
 
+
+def build_cmd(cd, cmd):
+    return "{} && {}".format(cd, cmd) if cmd else None
+
+
 def perform_step(step, repo, source, warnings, build_folder=None):
     tmp_cmd = ""
-    command = None
     cmd_fail = None
     if isinstance(step, list):
         args = step[1:]
@@ -45,7 +51,10 @@ def perform_step(step, repo, source, warnings, build_folder=None):
         tmp_cmd = "git clean -fXd"
     elif step == "rsync":
         build_folder = args[0]
-        command = "mkdir -p {dst} && rsync -r {root}/{repo} {dst}".format(root=source, repo=repo, dst=args[0])
+        run_command(
+            "mkdir -p {dst} && rsync -r {root}/{repo} {dst}".format(
+                root=source, repo=repo, dst=build_folder))
+        return build_folder
     elif step == "autogen":
         tmp_cmd = "./autogen.sh --enable-debug"
         if repo == "nova":
@@ -53,7 +62,7 @@ def perform_step(step, repo, source, warnings, build_folder=None):
     elif step == "make":
         tmp_cmd = "make -j2"
         if warnings:
-            tmp_cmd = "make -j2 CFLAGS=-Werror" # TODO: Default to this
+            tmp_cmd = "make -j2 CFLAGS=-Werror"  # TODO: Default to this
     elif step == "test":
         tmp_cmd = "cd tests/unit && make check"
         cmd_fail = "cd tests/unit && cat test-suite.log"
@@ -61,20 +70,24 @@ def perform_step(step, repo, source, warnings, build_folder=None):
         tmp_cmd = "make -j2 install"
     else:
         raise NotImplementedError()
-    if not command:
-        if build_folder:
-            source = build_folder
-        cmd_cd = "cd {src} && cd {repo}".format(src=source,repo=repo)
-        command = "{cd} && {tmp_cmd}".format(cd=cmd_cd,tmp_cmd=tmp_cmd)
-        cmd_fail = "{} && {}".format(cmd_cd, cmd_fail) if cmd_fail else None
+    if build_folder:
+        source = build_folder
+
+    cmd_cd = "cd {src} && cd {repo}".format(src=source, repo=repo)
+    command = build_cmd(cmd_cd, tmp_cmd)
+    cmd_fail = build_cmd(cmd_cd, cmd_fail)
+
     run_command(command, cmd_fail)
     return build_folder
+
 
 def build(steps, repos, source, warnings):
     build_folder = None
     for step in steps:
         for repo in repos:
-            build_folder = perform_step(step,repo,source,warnings,build_folder)
+            build_folder = perform_step(
+                step, repo, source, warnings, build_folder)
+
 
 def get_steps(args):
     steps = []
@@ -103,6 +116,7 @@ def get_steps(args):
         user_error("No build steps specified, see --help")
     return steps
 
+
 def get_repos(args):
     repos = []
     _all = args.build_all or args.all_repos
@@ -120,50 +134,76 @@ def get_repos(args):
         user_error("No repos specified")
     return repos
 
+
 def get_args():
-    ap = argparse.ArgumentParser(description="Developer script for building CFEngine enterprise",
-                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    build_local_path  = os.path.abspath(__file__)
+    ap = argparse.ArgumentParser(
+        description="Developer script for building CFEngine enterprise",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    build_local_path = os.path.abspath(__file__)
     buildscripts_path = os.path.dirname(build_local_path)
-    cfengine_path     = os.path.dirname(buildscripts_path)
+    cfengine_path = os.path.dirname(buildscripts_path)
 
     # ALL:
-    ap.add_argument("--build-all", help="Equiv: --build --all-repos", action="store_true")
-    ap.add_argument("--all-repos", help="Equiv: --core --masterfiles --enterprise --nova", action="store_true")
-    ap.add_argument("--build",     help="Equiv: --autogen --make", action="store_true")
+    ap.add_argument(
+        "--build-all", help="Equiv: --build --all-repos", action="store_true")
+    ap.add_argument(
+        "--all-repos",
+        help="Equiv: --core --masterfiles --enterprise --nova",
+        action="store_true")
+    ap.add_argument(
+        "--build", help="Equiv: --autogen --make", action="store_true")
 
     # STEPS:
     ap.add_argument("--checkout", help="Switch git branch", type=str)
-    ap.add_argument("--fetch",    help="Run fetch step",    action="store_true")
-    ap.add_argument("--rebase",   help="Rebase git branch", type=str)
-    ap.add_argument("--clean",    help="Run clean step",    action="store_true")
-    ap.add_argument("--rsync",    help="Rsync and run remaining commands in specified directory",   type=str)
-    ap.add_argument("--autogen",  help="Run autogen step",  action="store_true")
-    ap.add_argument("--make",     help="Run make step",     action="store_true")
-    ap.add_argument("--test",     help="Run make check",    action="store_true")
-    ap.add_argument("--install",  help="Run install step",  action="store_true")
-    ap.add_argument("--steps",    help="Steps (commands) to run", nargs="+")
+    ap.add_argument("--fetch", help="Run fetch step", action="store_true")
+    ap.add_argument("--rebase", help="Rebase git branch", type=str)
+    ap.add_argument("--clean", help="Run clean step", action="store_true")
+    ap.add_argument(
+        "--rsync",
+        help="Rsync and run remaining commands in specified directory",
+        type=str)
+    ap.add_argument("--autogen", help="Run autogen step", action="store_true")
+    ap.add_argument("--make", help="Run make step", action="store_true")
+    ap.add_argument("--test", help="Run make check", action="store_true")
+    ap.add_argument("--install", help="Run install step", action="store_true")
+    ap.add_argument("--steps", help="Steps (commands) to run", nargs="+")
 
     # REPOS:
-    ap.add_argument("--core",        help="Add core to --repos",        action="store_true")
-    ap.add_argument("--masterfiles", help="Add masterfiles to --repos", action="store_true")
-    ap.add_argument("--enterprise",  help="Add enterprise to --repos",  action="store_true")
-    ap.add_argument("--nova",        help="Add nova to --repos",        action="store_true")
-    ap.add_argument("--repos",       help="Repositories to run commands in", nargs="+")
+    ap.add_argument("--core", help="Add core to --repos", action="store_true")
+    ap.add_argument(
+        "--masterfiles",
+        help="Add masterfiles to --repos",
+        action='store_true')
+    ap.add_argument(
+        "--enterprise", help="Add enterprise to --repos", action="store_true")
+    ap.add_argument("--nova", help="Add nova to --repos", action="store_true")
+    ap.add_argument(
+        "--repos", help="Repositories to run commands in", nargs="+")
 
     # SOURCE:
-    ap.add_argument("--source", "-d", help="Where to look for CFEngine sources", type=str, default=cfengine_path)
+    ap.add_argument(
+        "--source",
+        help="Where to look for CFEngine sources",
+        type=str,
+        default=cfengine_path)
 
     # LOGGING:
-    ap.add_argument("--info",    help="Sets python loglevel to info",  action="store_true")
-    ap.add_argument("--verbose", help="Sets python loglevel to debug", action="store_true")
+    ap.add_argument(
+        "--info", help="Sets python loglevel to info", action="store_true")
+    ap.add_argument(
+        "--verbose",
+        help="Sets python loglevel to debug",
+        action="store_true")
 
     # BUILD TYPE:
-    ap.add_argument("--debug",   help="Build in debug mode",   action="store_true")
-    ap.add_argument("--release", help="Build in release mode", action="store_true")
+    ap.add_argument(
+        "--debug", help="Build in debug mode", action="store_true")
+    ap.add_argument(
+        "--release", help="Build in release mode", action="store_true")
 
     # MISC:
-    ap.add_argument("--dry-run", help="Show commands to be run", action="store_true")
+    ap.add_argument(
+        "--dry-run", help="Show commands to be run", action="store_true")
     ap.add_argument("--warnings", help="WIP: -Werror", action="store_true")
 
     args = ap.parse_args()
@@ -184,6 +224,7 @@ def get_args():
         args.debug = True
 
     return args
+
 
 if __name__ == "__main__":
     log.setLevel(logging.WARNING)
